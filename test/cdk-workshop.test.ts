c8fd093b41a34a048570e48828bc337e199d8377
yaroslavche <yaroslav429@gmail.com>
2019-11-20 13:29:15 +0200

    Test lambdas created

--- test/cdk-workshop.test.ts
+++ test/cdk-workshop.test.ts
@@ -1,21 +1,14 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
+import {expect as expectCDK, haveResource} from '@aws-cdk/assert';
 import cdk = require('@aws-cdk/core');
 import CdkWorkshop = require('../lib/cdk-workshop-stack');
 
-test('SQS Queue Created', () => {
+test('Lambdas created', () => {
     const app = new cdk.App();
-    // WHEN
     const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
-      VisibilityTimeout: 300
+    expectCDK(stack).to(haveResource('AWS::Lambda::Function', {
+        Handler: 'hello.handler'
     }));
-});
-
-test('SNS Topic Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
-  // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+    expectCDK(stack).to(haveResource('AWS::Lambda::Function', {
+        Handler: 'hitcounter.handler'
+    }));
 });
